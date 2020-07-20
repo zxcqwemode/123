db5c75597d0ec4435fc65670b4df37fed3b98307
Anzhelika Kravchuk <anzhelika.kravchuk@gmail.com>
2020-07-20 14:18:24 +0000

    Upload New File

---
+++ PrintFace/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace PrintFace
+{
+    public static class Program
+    {
+        /// <summary>
+        /// Print "Hello, world!" in a separate line.
+        /// </summary>
+        /// <param name="args">Command line arguments.</param>
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("  Hello, world! ");
+        }
+
+        /// <summary>
+        /// Print "Hello, userName!" in a separate line.
+        /// </summary>
+        /// <param name="userName">Name of user.</param>
+        public static void SayHelloUser(string userName)
+        {
+            throw new NotImplementedException();
+        }
+        
+        /// <summary>
+        /// Print this face.
+        ///   +"""""+
+        ///  (| o o |)
+        ///   |  ^  |
+        ///   | '-' |
+        ///   +-----+
+        /// </summary>
+        public static void PrintFace()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
